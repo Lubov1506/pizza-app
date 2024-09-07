dc52cc7a30113d983ff1e37c4948012cf973a1e4
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-09-07 15:37:08 +0300

    search

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -5,6 +5,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "../ui";
 import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import Link from "next/link";
+import { SearchInput } from "./search-input";
 interface Props {
   className?: string;
 }
@@ -12,15 +14,21 @@ export const Header: React.FC<Props> = ({ className }) => {
   return (
     <div className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        <div className="flex items-center gap-4">
-          <Image src="/logo.png" alt="Logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">
-              can&apos;t be taster
-            </p>
+        <Link href="/">
+          <div className="flex items-center gap-4">
+            <Image src="/logo.png" alt="Logo" width={35} height={35} />
+            <div>
+              <h1 className="text-2xl uppercase font-black">Pizza</h1>
+              <p className="text-sm text-gray-400 leading-3">
+                can&apos;t be taster
+              </p>
+            </div>
           </div>
-        </div>
+        </Link>
+
+    <div className="mx-10 flex-1">
+      <SearchInput/>
+    </div>
 
         <div className="flex items-center gap-3">
           <Button variant="outline" className="flex items-center gap-1">
--- prisma/constants.ts
+++ prisma/constants.ts
@@ -1,200 +1,217 @@
 export const categories = [
-    {
-      name: 'Pizzas',
-    },
-    {
-      name: 'Breakfast',
-    },
-    {
-      name: 'Snacks',
-    },
-    {
-      name: 'Cocktails',
-    },
-    {
-      name: 'Drinks',
-    },
-  ];
-  
-  export const _ingredients = [
-    {
-      name: 'Cheese border',
-      price: 179,
-      imageUrl:
-        'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png',
-    },
-    {
-      name: 'Cream cheese',
-      price: 79,
-      imageUrl:
-        'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png',
-    },
-    {
-      name: 'Cheddar and parmesan cheese',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796',
-    },
-    {
-      name: 'Spicy pepper jalapeno',
-      price: 59,
-      imageUrl:
-        'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png',
-    },
-    {
-      name: 'Tender chicken',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA5B328D35A',
-    },
-    {
-      name: 'Mushrooms',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA67259A324',
-    },
-    {
-      name: 'Ham',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA61B9A8D61',
-    },
-    {
-      name: 'Spicy pepperoni',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA6258199C3',
-    },
-    {
-      name: 'Hot chorizo',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA62D5D6027',
-    },
-    {
-      name: 'Marinated cucumbers',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9EA89958D782B',
-    },
-    {
-      name: 'Fresh tomatoes',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA7AC1A1D67',
-    },
-    {
-      name: 'Red onion',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C',
-    },
-    {
-      name: 'Juicy pineapple',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0',
-    },
-    {
-      name: 'Italian herbs',
-      price: 39,
-      imageUrl:
-        'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png',
-    },
-    {
-      name: 'Sweet pepper',
-      price: 59,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B',
-    },
-    {
-      name: 'Feta cheese cubes',
-      price: 79,
-      imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A39D824A82E11E9AFA6B0FFC349',
-    },
-    {
-      name: 'Meatballs',
-      price: 79,
-      imageUrl:
-        'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png',
-    },
-  ].map((obj, index) => ({ id: index + 1, ...obj }));
-  
-  export const products = [
-    {
-      name: 'Omelette with ham and mushrooms',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
-      categoryId: 2,
-    },
-    {
-      name: 'Omelette with pepperoni',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
-      categoryId: 2,
-    },
-    {
-      name: 'Coffee Latte',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
-      categoryId: 2,
-    },
-    {
-      name: 'Sandwich with ham and cheese',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
-      categoryId: 3,
-    },
-    {
-      name: 'Chicken nuggets',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
-      categoryId: 3,
-    },
-    {
-      name: 'Potatoes with sauce🌱',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
-      categoryId: 3,
-    },
-    {
-      name: 'Dodster',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
-      categoryId: 3,
-    },
-    {
-      name: 'Spicy Dodster🌶️🌶️',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
-      categoryId: 3,
-    },
-    {
-      name: 'Banana milkshake',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
-      categoryId: 4,
-    },
-    {
-      name: 'Caramel apple milkshake',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
-      categoryId: 4,
-    },
-    {
-      name: 'Milkshake with Oreo cookies',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
-      categoryId: 4,
-    },
-    {
-      name: 'Classic milkshake👶',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
-      categoryId: 4,
-    },
-    {
-      name: 'Irish Cappuccino',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
-      categoryId: 5,
-    },
-    {
-      name: 'Caramel Cappuccino',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
-      categoryId: 5,
-    },
-    {
-      name: 'Coconut Latte',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
-      categoryId: 5,
-    },
-    {
-      name: 'Americano',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
-      categoryId: 5,
-    },
-    {
-      name: 'Latte',
-      imageUrl: 'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
-      categoryId: 5,
-    },
-  ];
-  
+  {
+    name: "Pizzas",
+  },
+  {
+    name: "Breakfast",
+  },
+  {
+    name: "Snacks",
+  },
+  {
+    name: "Cocktails",
+  },
+  {
+    name: "Drinks",
+  },
+];
+
+export const _ingredients = [
+  {
+    id: 1,
+    name: "Cheese border",
+    price: 179,
+    imageUrl: "/images/ingredients/ingredient-1.png",
+  },
+  {
+    id: 2,
+    name: "Cream cheese",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-2.png",
+  },
+  {
+    id: 3,
+    name: "Cheddar and parmesan cheese",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-3.png",
+  },
+  {
+    id: 4,
+    name: "Spicy pepper jalapeno",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-4.png",
+  },
+  {
+    id: 5,
+    name: "Tender chicken",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-5.png",
+  },
+  {
+    id: 6,
+    name: "Mushrooms",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-6.png",
+  },
+  {
+    id: 7,
+    name: "Ham",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-7.png",
+  },
+  {
+    id: 8,
+    name: "Spicy pepperoni",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-8.png",
+  },
+  {
+    id: 9,
+    name: "Hot chorizo",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-9.png",
+  },
+  {
+    id: 10,
+    name: "Marinated cucumbers",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-10.png",
+  },
+  {
+    id: 11,
+    name: "Fresh tomatoes",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-11.png",
+  },
+  {
+    id: 12,
+    name: "Red onion",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-12.png",
+  },
+  {
+    id: 13,
+    name: "Juicy pineapple",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-13.png",
+  },
+  {
+    id: 14,
+    name: "Italian herbs",
+    price: 39,
+    imageUrl: "/images/ingredients/ingredient-14.png",
+  },
+  {
+    id: 15,
+    name: "Sweet pepper",
+    price: 59,
+    imageUrl: "/images/ingredients/ingredient-15.png",
+  },
+  {
+    id: 16,
+    name: "Feta cheese cubes",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-16.png",
+  },
+  {
+    id: 17,
+    name: "Meatballs",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-17.png",
+  },
+  {
+    id: 18,
+    name: "Mozzarella",
+    price: 79,
+    imageUrl: "/images/ingredients/ingredient-18.png",
+  },
+].map((obj, index) => ({ id: index + 1, ...obj }));
+
+export const products = [
+  {
+    name: "Omelette with ham and mushrooms",
+    imageUrl: "/images/products/product-1.webp",
+    categoryId: 2,
+  },
+  {
+    name: "Omelette with pepperoni",
+    imageUrl: "/images/products/product-2.webp",
+    categoryId: 2,
+  },
+  {
+    name: "Coffee Latte",
+    imageUrl: "/images/products/product-3.webp",
+    categoryId: 2,
+  },
+  {
+    name: "Sandwich with ham and cheese",
+    imageUrl: "/images/products/product-4.webp",
+    categoryId: 3,
+  },
+  {
+    name: "Chicken nuggets",
+    imageUrl: "/images/products/product-5.webp",
+    categoryId: 3,
+  },
+  {
+    name: "Potatoes with sauce",
+    imageUrl: "/images/products/product-6.webp",
+    categoryId: 3,
+  },
+  {
+    name: "Dodster",
+    imageUrl: "/images/products/product-7.webp",
+    categoryId: 3,
+  },
+  {
+    name: "Spicy Dodster",
+    imageUrl: "/images/products/product-8.webp",
+    categoryId: 3,
+  },
+  {
+    name: "Banana milkshake",
+    imageUrl: "/images/products/product-9.webp",
+    categoryId: 4,
+  },
+  {
+    name: "Caramel apple milkshake",
+    imageUrl: "/images/products/product-10.webp",
+    categoryId: 4,
+  },
+  {
+    name: "Milkshake with Oreo cookies",
+    imageUrl: "/images/products/product-11.webp",
+    categoryId: 4,
+  },
+  {
+    name: "Classic milkshake",
+    imageUrl: "/images/products/product-12.webp",
+    categoryId: 4,
+  },
+  {
+    name: "Irish Cappuccino",
+    imageUrl: "/images/products/product-13.webp",
+    categoryId: 5,
+  },
+  {
+    name: "Caramel Cappuccino",
+    imageUrl: "/images/products/product-14.webp",
+    categoryId: 5,
+  },
+  {
+    name: "Coconut Latte",
+    imageUrl: "/images/products/product-15.webp",
+    categoryId: 5,
+  },
+  {
+    name: "Americano",
+    imageUrl: "/images/products/product-16.webp",
+    categoryId: 5,
+  },
+  {
+    name: "Latte",
+    imageUrl: "/images/products/product-17.webp",
+    categoryId: 5,
+  },
+];
 
